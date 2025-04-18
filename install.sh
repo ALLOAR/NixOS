6c8584e23af383cd8af71ef2286d73c9c493d8e1
ALLOAR <evgenzbiranik@gmail.com>
2025-04-18 18:28:29 +0200

    added auto install

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+DISK="/dev/sda"  # ❗ Измени только это
+
+echo "[1/5] Разметка $DISK..."
+parted $DISK -- mklabel gpt
+parted $DISK -- mkpart ESP fat32 1MiB 512MiB
+parted $DISK -- set 1 boot on
+parted $DISK -- mkpart primary ext4 512MiB 100%
+
+mkfs.fat -F32 -n boot ${DISK}1
+mkfs.ext4 -L nixos ${DISK}2
+
+echo "[2/5] Монтирование..."
+mount ${DISK}2 /mnt
+mkdir -p /mnt/boot
+mount ${DISK}1 /mnt/boot
+
+echo "[3/5] Копирование конфигов..."
+cp -r /mnt-root/nixos/* /mnt/etc/nixos/
+
+echo "[4/5] Установка..."
+nixos-install --no-root-passwd
+
+echo "[5/5] Перезагрузка..."
+sleep 5
+reboot
