b524e501de5b059ea3297f3607ed61fcf703fe06
ALLOAR <evgenzbiranik@gmail.com>
2025-04-20 23:01:40 +0200

    new version of installer

---
+++ 2nix_installer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+cd
+echo "  [1/ ] "
+if [ -d "/etc/nixos/configurations" ]; then
+    echo "Директория configurations существует"
+    echo "Установка Конфигураций не нужна"
+else
+    mkdir -p /etc/nixos/configurations
+    curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configuration.nix -o /mnt/etc/nixos/configurations/configuration.nix
+    curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/programs.nix -o /mnt/etc/nixos/configurations/programs.nix
+    curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/amd.nix -o /mnt/etc/nixos/configurations/amd.nix
+    curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia.nix -o /mnt/etc/nixos/configurations/nvidia.nix
+    curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia_prime.nix -o /mnt/etc/nixos/configurations/nvidia_prime.nix
+fi
+cd
+if [ -d "~/.config/hypr" ]; then
+    echo "Директория существует"
+else
+    mkdir -p ~/.config
+    mkdir -p ~/.config/hypr/
+    mkdir -p ~/.config/home-manager
+fi
+
+echo "  [2/ ] "
+cd ~/.config/home-manager
+git clone --branch home --single-branch https://github.com/ALLOAR/NixOS
+cd NixOS
+cp -r * ~/.config/home-manager
+cd ~/.config/home-manager
+rm -rf NixOS
+
