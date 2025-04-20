ca7eea9c94900aea647840f8344c4f73a78f7f5c
ALLOAR <evgenzbiranik@gmail.com>
2025-04-20 23:06:42 +0200

    modified that better

--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@ set -e
 
 DISK="/dev/sda"  # ❗ Измени только это
 
-echo " you realy want erase this disk?"
-
-read -p "$DISK" p
-
-rm -rf ~/*
-
 echo "[1/5] Разметка $DISK..."
 parted $DISK -- mklabel gpt
 parted $DISK -- mkpart ESP fat32 1MiB 1500MiB
@@ -19,9 +13,6 @@ mkfs.fat -F32 -n boot ${DISK}1
 mkfs.ext4 -L nixos ${DISK}2
 
 echo "[2/5] Монтирование..."
-
-sudo mkdir -p /mnt
-
 mount ${DISK}2 /mnt
 mkdir -p /mnt/boot
 mount ${DISK}1 /mnt/boot
@@ -30,37 +21,29 @@ echo "[3/5] Копирование конфигов..."
 
 cd
 
-cp /etc/configuration.nix ~/
-cp -r /etc/configurations/ ~/
-
 sudo nixos-generate-config --root /mnt
 
-#curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configuration.nix -o $HOME/configuration.nix
-#curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/programs.nix -o $HOME/programs.nix
-#curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/amd.nix -o $HOME/amd.nix
-#curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia.nix -o $HOME/nvidia.nix
-#curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia_prime.nix -o $HOME/nvidia_prime.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configuration.nix -o $HOME/configuration.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/programs.nix -o $HOME/programs.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/amd.nix -o $HOME/amd.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia.nix -o $HOME/nvidia.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia_prime.nix -o $HOME/nvidia_prime.nix
 
 
 cd /mnt/etc/nixos
 sudo mkdir -p configurations
 cd
 
-
+sudo mv programs.nix /mnt/etc/nixos/configurations/
 sudo mv configuration.nix /mnt/etc/nixos/
-sudo mv configurations /mnt/etc/nixos
-#sudo mv ./configurations/programs.nix /mnt/etc/nixos/configurations/
-#sudo mv ./configurations/amd.nix /mnt/etc/nixos/configurations/
-#sudo mv ./configurations/nvidia.nix /mnt/etc/nixos/configurations/
-#sudo mv ./configurations/nvidia_prime.nix /mnt/etc/nixos/configurations/
-
+sudo mv amd.nix /mnt/etc/nixos/configurations/
+sudo mv nvidia.nix /mnt/etc/nixos/configurations/
+sudo mv nvidia_prime.nix /mnt/etc/nixos/configurations/
 
 cd /mnt/etc/nixos/
 cp hardware-configuration.nix ~/
 sudo mv hardware-configuration.nix configurations
 cd
-
-nix-channel --add https://nixos.org/channels/nixos-24.11 nixos nix-channel --update
 
 echo "[4/4] Установка..."
 
