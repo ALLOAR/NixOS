3e5193b220e9fee419325eddf8d991cc4680d319
ALLOAR <evgenzbiranik@gmail.com>
2025-04-20 23:14:55 +0200

    i f*ck forgot to save this shit

--- install.sh
+++ install.sh
@@ -5,40 +5,27 @@ DISK="/dev/sda"  # ❗ Измени только это
 
 echo "[1/5] Разметка $DISK..."
 parted $DISK -- mklabel gpt
-parted $DISK -- mkpart ESP fat32 1MiB 1500MiB
+parted $DISK -- mkpart ESP fat32 1MiB 512MiB
 parted $DISK -- set 1 boot on
-parted $DISK -- mkpart primary ext4 1500MiB 100%
+parted $DISK -- mkpart primary ext4 512MiB 100%
 
 mkfs.fat -F32 -n boot ${DISK}1
 mkfs.ext4 -L nixos ${DISK}2
 
 echo "[2/5] Монтирование..."
+sudo mkdir -p /mnt
 mount ${DISK}2 /mnt
 mkdir -p /mnt/boot
 mount ${DISK}1 /mnt/boot
-
 echo "[3/5] Копирование конфигов..."
-
 cd
-
 sudo nixos-generate-config --root /mnt
-
-curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configuration.nix -o $HOME/configuration.nix
-curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/programs.nix -o $HOME/programs.nix
-curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/amd.nix -o $HOME/amd.nix
-curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia.nix -o $HOME/nvidia.nix
-curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia_prime.nix -o $HOME/nvidia_prime.nix
-
-
-cd /mnt/etc/nixos
-sudo mkdir -p configurations
-cd
-
-sudo mv programs.nix /mnt/etc/nixos/configurations/
-sudo mv configuration.nix /mnt/etc/nixos/
-sudo mv amd.nix /mnt/etc/nixos/configurations/
-sudo mv nvidia.nix /mnt/etc/nixos/configurations/
-sudo mv nvidia_prime.nix /mnt/etc/nixos/configurations/
+mkdir -p /mnt/etc/nixos/configurations
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configuration.nix -o /mnt/etc/nixos/configuration.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/programs.nix -o /mnt/etc/nixos/configurations/programs.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/amd.nix -o /mnt/etc/nixos/configurations/amd.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia.nix -o /mnt/etc/nixos/configurations/nvidia.nix
+curl https://raw.githubusercontent.com/ALLOAR/NixOS/refs/heads/main/configurations/nvidia_prime.nix -o /mnt/etc/nixos/configurations/nvidia_prime.nix
 
 cd /mnt/etc/nixos/
 cp hardware-configuration.nix ~/
