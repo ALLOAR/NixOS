09a365963bc52b8786596c728b2123eb53c01dca
ALLOAR <evgenzbiranik@gmail.com>
2026-02-08 00:34:11 +0100

    added a laptop battery cpu control

---
+++ other/laptop.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }: {
+
+services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+	
+	#minimal/maximal boost cpu on power charging
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+	#minimal/maximal boost cpu on battery
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 40;
+
+       #Optional helps save long term battery health
+       START_CHARGE_THRESH_BAT0 = 60; # 40 and below it starts to charge
+       STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
+
+      };
+};
