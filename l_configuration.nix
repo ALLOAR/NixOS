8eb8b889bd66c23c316dc7c42a04f63707ff6ddb
ALLOAR <evgenzbiranik@gmail.com>
2026-02-10 15:37:45 +0100

    fixed lateest laptop things

--- l_configuration.nix
+++ l_configuration.nix
@@ -4,6 +4,7 @@
   imports = [ 
 	./hardware-configuration.nix
         ./nixos-hardware/lenovo/ideapad/15ach6/default.nix
+	./other/laptop.nix
 	./other/packages.nix
 	./other/stuff.nix
 	./other/config.nix
