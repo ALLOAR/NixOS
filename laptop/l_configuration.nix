f38ac8acbce889258bd0369b40ecf2d2d948eeb9
ALLOAR <evgenzbiranik@gmail.com>
2026-03-20 21:12:08 +0100

    a

--- laptop/l_configuration.nix
+++ laptop/l_configuration.nix
@@ -5,7 +5,7 @@
 	./hardware-configuration.nix
     ../nixos-hardware/lenovo/ideapad/15ach6/default.nix
 	../other/packages.nix
-	../other/stuff.nix
+#	../other/stuff.nix
 	../other/config.nix
     ];
 }
