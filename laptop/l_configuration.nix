270d6a33d7cd5a3c52cc10e252ae22d3b5121808
ALLOAR <evgenzbiranik@gmail.com>
2026-03-20 19:55:32 +0100

    a

--- laptop/l_configuration.nix
+++ laptop/l_configuration.nix
@@ -3,11 +3,10 @@
 {
   imports = [ 
 	./hardware-configuration.nix
-        ../nixos-hardware/lenovo/ideapad/15ach6/default.nix
+    ../nixos-hardware/lenovo/ideapad/15ach6/default.nix
 	../other/packages.nix
 	../other/stuff.nix
 	../other/config.nix
-	../other/swap.nix	
     ];
 }
 
